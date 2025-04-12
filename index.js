9ab2e442631887904dc495a0354eb4faadd09f60
loldlobi <macska5552@gmail.com>
2025-04-12 19:48:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-app.use(cors())
-app.use(express.json())
+// Increase payload size limit
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(cors());
 
 app.use("/userapi", require('./routes/userRoutes'));
 app.use("/termekapi",require('./routes/termekekRoute'))
